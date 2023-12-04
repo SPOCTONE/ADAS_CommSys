ff70e4cfacaa5956e85ff02ad0b28ca525e0ceed
Subham Patel <sp_octone@outlook.com>
2023-12-04 09:40:43 +0530

    Update ACCreceive.c

--- ACCreceive.c
+++ ACCreceive.c
@@ -12,31 +12,29 @@ typedef struct car {
 } ACC;
 
 int main() {
-  printf("\nReceiver Function:\n");
   ACC receive;
-  float speed, distance, threshold_distance = 100;
+  printf("\nReceiver Function:\n");
   int msgid = msgget((key_t)1, 0666 | IPC_CREAT);
+  
   
   if((msgrcv(msgid, (void *)&receive, 0, 0, 0)) == -1) {
   perror("msgrcv");
   }
   
-  speed = receive.speed;
-  printf("The speed of our car is: %.2f kmph\n", speed);
-  float threshold = 2*(speed/3.6); //atleast two seconds behind the vehicle in front
-  printf("Minimum safe distance between two cars on a highway at this speed should be: %.2f metres\n", threshold);
-  distance = receive.distance;
-  printf("The distance between us and the car in front is: %.2f m\n", distance);
+  printf("The speed of our car is: %.2f kmph\n", receive.speed);
+  float threshold = 2*(receive.speed/3.6); //atleast two seconds behind the vehicle in front
+  printf("Minimum safe distance between two cars on a highway at this receive.speed should be: %.2f metres\n", threshold);
+  printf("The distance between us and the car in front is: %.2f m\n", receive.distance);
   
   if((msgctl(msgid, IPC_RMID, NULL)) == -1) {
   perror("msgctl");
   };
   
-  if (distance >= threshold) {
+  if (receive.distance >= threshold) {
     printf("\nWe will continue cruising at the same speed\n");
   } 
-  else if (distance < threshold) {
-    printf("\nWe will need to change the cruising speed in order to maintain a safe distance from the car in front\n");
+  else if (receive.distance < threshold) {
+    printf("\nWe will need to change the cruising receive.speed in order to maintain a safe distance from the car in front\n");
   }
   
   execl("/home/sys1/Project/menu", "menu", NULL);
